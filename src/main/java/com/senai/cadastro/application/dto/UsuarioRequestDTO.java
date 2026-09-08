e2f6541f4dc9d86ef9ccd0d4cd39a175f963ba40
Joao-Victor-de-Souza-Marinho <177157496+Joao-Victor-de-Souza-Marinho@users.noreply.github.com>
2026-09-08 08:44:26 -0300

    corrections in dto

--- src/main/java/com/senai/cadastro/application/dto/UsuarioRequestDTO.java
+++ src/main/java/com/senai/cadastro/application/dto/UsuarioRequestDTO.java
@@ -2,9 +2,7 @@ package com.senai.cadastro.application.dto;
 
 import com.senai.cadastro.domain.entity.Usuario;
 
-
 public record UsuarioRequestDTO(
-        
         String nome,
         String cpf,
         String email,
@@ -13,12 +11,11 @@ public record UsuarioRequestDTO(
 
     public Usuario toEntity() {
         return new Usuario(
-                id:null,
-                dto.nome,
-                dto.cpf,
-                dto.email,
-                dto.senha
-            );
+                null,
+                senha,
+                nome,
+                cpf,
+                email
+        );
     }
 }
-
